a1407b7759d6402ee96f5e0a264e3aa46487d89b
Lilija Kazutina <Lilija.Kazutina@exigenservices.com>
2018-04-10 19:05:52 +0300

    Redundant parameters from search controller are removed; Room count buttons are fixed

--- bachelorPaper/Controllers/SearchViewController.m
+++ bachelorPaper/Controllers/SearchViewController.m
@@ -34,31 +34,17 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 9;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     switch (section) {
         case 0:
-            return 8;
+            return 4;
         case 1:
-            return 1;
+            return 3;
         case 2:
             return 4;
-        case 3:
-            return 3;
-        case 4:
-            return 1;
-        case 5:
-            return 3;
-        case 6:
-            return 3;
-        case 7:
-            return 2;
-        case 8:
-            return 4;
-        case 9:
-            return 2;
         default:
             return 1;
     }
@@ -67,185 +53,64 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     switch (indexPath.section) {
         case 0: {
-                switch (indexPath.row) {
-                    case 0: {
-                        SegmentedControlTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SegementedControlCellIdentifier" forIndexPath:indexPath];
-                        return cell;
-                    }
-                    case 1: {
-                        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                        cell.textLabel.text = AMLocalizedString(@"category", nil);
-                        return cell;
-                    }
-                    case 2: {
-                        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                        cell.textLabel.text = AMLocalizedString(@"type", nil);
-                        return cell;
-                    }
-                    case 3: {
-                        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                        cell.textLabel.text = AMLocalizedString(@"location", nil);
-                        return cell;
-                    }
-                    case 4: {
-                        RoomCountParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RoomCountParameterCellIdentifier" forIndexPath:indexPath];
-                        return cell;
-                    }
-                    case 5: {
-                        SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                        cell.parameterLabel.text = AMLocalizedString(@"studio", nil);
-                        return cell;
-                    }
-                    case 6: {
-                        FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                        cell.parameterLabel.text = AMLocalizedString(@"price", nil);
-                        return cell;
-                    }
-                    case 7: {
-                        SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                        cell.parameterLabel.text = AMLocalizedString(@"with_photos", nil);
-                        return cell;
-                    }
-                    default:
-                        break;
+            switch (indexPath.row) {
+                case 0: {
+                    SegmentedControlTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SegementedControlCellIdentifier" forIndexPath:indexPath];
+                    return cell;
                 }
+                case 1: {
+                    RoomCountParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RoomCountParameterCellIdentifier" forIndexPath:indexPath];
+                    return cell;
+                }
+                case 2: {
+                    SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
+                    cell.parameterLabel.text = AMLocalizedString(@"studio", nil);
+                    return cell;
+                }
+                case 3: {
+                    FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
+                    cell.parameterLabel.text = AMLocalizedString(@"price", nil);
+                    return cell;
+                }
+                default:
+                    break;
+            }
         }
         case 1: {
             if (indexPath.row == 0) {
+                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
+                cell.textLabel.text = AMLocalizedString(@"city", nil);
+                return cell;
+            } else if (indexPath.row == 1) {
                 UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
                 cell.textLabel.text = AMLocalizedString(@"district", nil);
                 return cell;
             } else {
                 UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
                 cell.textLabel.text = AMLocalizedString(@"street", nil);
+                return cell;
             }
         }
         case 2: {
-            if (indexPath.row == 1) {
-                SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                cell.parameterLabel.text = AMLocalizedString(@"not_first", nil);
-                return cell;
-            } else if (indexPath.row == 2) {
-                SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                cell.parameterLabel.text = AMLocalizedString(@"not_last", nil);
-                return cell;
-            } else if (indexPath.row == 0) {
+            if (indexPath.row == 0) {
                 FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
                 cell.parameterLabel.text = AMLocalizedString(@"floor",  nil);
                 return cell;
-            } else if (indexPath.row == 3) {
+            } else if (indexPath.row == 1) {
                 FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
-               cell.parameterLabel.text = AMLocalizedString(@"floor_count", nil);
+                cell.parameterLabel.text = AMLocalizedString(@"total_area", nil);
                 return cell;
-            }
-            }
-        case 3: {
-            switch (indexPath.row) {
-                case 0: {
-                    FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                     cell.parameterLabel.text = AMLocalizedString(@"total_area", nil);
-                    return cell;
-                }
-                case 1: {
-                    FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                     cell.parameterLabel.text = AMLocalizedString(@"living_area", nil);
-                    return cell;
-                }
-                case 2: {
-                    FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                     cell.parameterLabel.text = AMLocalizedString(@"kitchen_area", nil);
-                    return cell;
-                }
-                default:
-                    break;
-            }
-        }
-        case 4: {
-            FromToParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FromToParameterTableViewCellIdentifier" forIndexPath:indexPath];
-             cell.parameterLabel.text = AMLocalizedString(@"year_of_construction", nil);
-            return cell;
-        }
-
-        case 5: {
-            if (indexPath.row == 0) {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"building_material", nil);
-                return cell;
-            } else if (indexPath.row == 1) {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"building_type", nil);
-                return cell;
-            } else {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"building_class", nil);
-                return cell;
-            }
-        }
-        case 6: {
-            if (indexPath.row == 0) {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"layout", nil);
-                return cell;
-            } else if (indexPath.row == 1) {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"bathroom", nil);
-                return cell;
-            } else {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"apartment_type", nil);
-                return cell;
-            }
-        }
-        case 7: {
-            if (indexPath.row == 0) {
+            } else if (indexPath.row == 2) {
                 SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
                 cell.parameterLabel.text = AMLocalizedString(@"balcony", nil);
                 return cell;
             } else {
-                SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                cell.parameterLabel.text = AMLocalizedString(@"loggia", nil);
-                return cell;
-            }
-        }
-        case 8: {
-            switch (indexPath.row) {
-                case 0: {
-                    SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                    cell.parameterLabel.text = AMLocalizedString(@"bargain_possible", nil);
-                    return cell;
-                }
-                case 1: {
-                    SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                    cell.parameterLabel.text = AMLocalizedString(@"mortgage_possible", nil);
-                    return cell;
-                }
-                case 2: {
-                    SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                    cell.parameterLabel.text = AMLocalizedString(@"change", nil);
-                    return cell;
-                }
-                case 3: {
-                    SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-                    cell.parameterLabel.text = AMLocalizedString(@"clean_sale", nil);
-                    return cell;
-                }
-                default:
-                    break;
-            }
-            SwitchParameterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SwitchParameterTableViewCellIdentifier" forIndexPath:indexPath];
-            return cell;
-        }
-        case 9: {
-            if (indexPath.row == 0) {
                 UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"author", nil);
-                return cell;
-            } else {
-                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"simpleParameterCellIdentifier" forIndexPath:indexPath];
-                cell.textLabel.text = AMLocalizedString(@"date", nil);
+                cell.textLabel.text = AMLocalizedString(@"heating_type", nil);
                 return cell;
             }
         }
+
         default: {
             UITableViewCell *cell;
             return cell;
